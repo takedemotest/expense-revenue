3c8ef95d59780f8b700e7e4dbfa6c578805cbb39
takedemotest <takedemotest@gmail.com>
2026-07-30 22:46:47 +0530

    redeploed

--- federation.config.mjs
+++ federation.config.mjs
@@ -3,6 +3,8 @@ import { withNativeFederation, shareAll } from '@angular-architects/native-feder
 export default withNativeFederation({
     name: 'expenseRevenue',
 
+    publicPath: 'https://takedemotest.github.io/expense-revenue/',
+
   exposes: {
     './expenseRevenueModule': './src/app/expense-revenue-component/expense-revenue-component.ts',
   },
